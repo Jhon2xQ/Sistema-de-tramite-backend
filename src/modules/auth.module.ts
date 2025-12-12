c2a8f9160bfc815031e39e9e47660fa7deb25258
Jhon2xQ <jhonq2512@gmail.com>
2025-12-12 12:33:14 -0500

    config del modulo para injeccion de JwtUtil

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PersistanceModule } from './persistance.module';
 import { AuthController } from 'src/controllers/auth.controller';
 import { AuthService } from 'src/services/auth.service';
+import { JwtUtil } from 'src/common/utils/jwt.util';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PersistanceModule],
+  imports: [PersistanceModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtUtil],
 })
 export class AuthModule {}
