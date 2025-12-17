a7a90e753a27459117ff304be023d49851fe6a69
Jhon2xQ <jhonq2512@gmail.com>
2025-12-16 19:17:43 -0500

    actualizando inyeccion de dependecias de auth

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from 'src/controllers/auth.controller';
 import { AuthService } from 'src/services/auth.service';
 import { JwtUtil } from 'src/common/utils/jwt.util';
 import { JwtModule } from '@nestjs/jwt';
+import { CookieUtil } from 'src/common/utils/cookie.util';
 
 @Module({
   imports: [PersistanceModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, JwtUtil],
+  providers: [AuthService, JwtUtil, CookieUtil],
+  exports: [JwtUtil],
 })
 export class AuthModule {}
