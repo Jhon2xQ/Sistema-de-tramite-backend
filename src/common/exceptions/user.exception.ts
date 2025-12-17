acdf120124d5e40453ebbc343df4ce88390bc0c2
Jhon2xQ <jhonq2512@gmail.com>
2025-12-17 12:57:54 -0500

    estandarizando exceptions

--- src/common/exceptions/user.exception.ts
+++
@@ -1,9 +0,0 @@
-import { DomainException } from './base.exception';
-
-export class NotFoundUserException extends DomainException {
-  readonly statusCode = 401;
-  readonly errors = null;
-  constructor(message: string) {
-    super(message);
-  }
-}
