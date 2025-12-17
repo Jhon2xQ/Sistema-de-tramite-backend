1da03fb37dab473d8f8286f0e4bc252ed010b9d0
Jhon2xQ <jhonq2512@gmail.com>
2025-12-16 19:19:33 -0500

    integrando guardia de access token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/accessJwt.guard';
 
 @Module({
   imports: [AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
