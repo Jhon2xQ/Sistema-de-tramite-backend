3e4e0b21bb760d9a2a3fbf1351e2c54388a2e309
Jhon2xQ <jhonq2512@gmail.com>
2025-12-18 09:59:20 -0500

    añadido exepcion para usuario desactivado

--- src/common/exceptions/auth.exception.ts
+++ src/common/exceptions/auth.exception.ts
@@ -4,6 +4,8 @@ import { DomainException } from './base.exception';
 export const AuthException = {
   invalidCredentials: (message = 'Credenciales inválidas') => DomainException.unauthorized(message),
 
+  userIsDesactivate: (message = 'El usuario esta desactivado') => DomainException.unauthorized(message),
+
   userAlreadyExists: (message = 'El usuario ya existe') => DomainException.conflict(message),
 
   invalidPassword: (message = 'Contraseña inválida') => DomainException.badRequest(message),
